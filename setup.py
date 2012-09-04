2acc4b79785ba324c3fd444d9a5c902e313d12fe
Chris Jesse <jesse1932@gmail.com>
2012-09-04 10:24:05 +0100

    Added package requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Flight Data Services Ltd
+# http://www.flightdataservices.com
+# See the file "LICENSE" for the full license governing this code.
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import stepper_motor as pkg
+from requirements import RequirementsParser
+requirements = RequirementsParser()
+
+setup(
+    name=pkg.__packagename__,
+    version=pkg.__version__,
+    author=pkg.__author__,
+    author_email=pkg.__author_email__,
+    maintainer=pkg.__maintainer__,
+    maintainer_email=pkg.__maintainer_email__,
+    url=pkg.__url__,
+    description=pkg.__description__,
+    long_description=open('README.rst').read() + open('CHANGES').read() +
+    open('TODO').read() + open('AUTHORS').read(),
+    download_url=pkg.__download_url__,
+    classifiers=pkg.__classifiers__,
+    platforms=pkg.__platforms__,
+    license=pkg.__license__,
+    keywords=pkg.__keywords__,
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements.install_requires,
+    setup_requires=requirements.setup_requires,
+    tests_require=requirements.tests_require,
+    extras_require=requirements.extras_require,
+    dependency_links=requirements.dependency_links,
+    test_suite='nose.collector',
+)
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
